2c9e7cb0f6e05c167ee6cf27beab72377bd165a5
brandonbellanti <brandonbellanti@gmail.com>
2020-03-17 16:24:46 -0400

    Updated directories

--- test.js
+++
@@ -1,7 +0,0 @@
-const token = '1d0167ef5a0d56b0abd7227042d8c92aeb52e251';
-
-fetch('https://api.esv.org/v3/passage/text/?q=Psalm+1', {
-  headers: {
-    Authorization: `Token ${token}`
-  }
-}).then(res => res.json()).then(json => console.log(json));
